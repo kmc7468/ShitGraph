e3702cba00a45b338d04ba8395546fdee721cd71
kmc7468 <kmc7468@naver.com>
2020-04-11 03:21:06 +0900

    WinMain 함수 추가

---
+++ src/Main.cpp
@@ -0,0 +1,104 @@
+#include <ShitGraph/CoreType.hpp>
+#include <ShitGraph/Graph.hpp>
+#include <ShitGraph/Graphs.hpp>
+#include <ShitGraph/platform/win32/Gdiplus.hpp>
+#include <ShitGraph/platform/win32/Graphic.hpp>
+#include <ShitGraph/platform/win32/Render.hpp>
+
+#include <Windows.h>
+
+ShitGraph::Graphs g_Graphs;
+
+POINT g_MousePos;
+bool g_IsMoving = false;
+
+LRESULT CALLBACK WndProc(HWND handle, UINT message, WPARAM wParam, LPARAM lParam);
+
+int APIENTRY WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int cmdShow) {
+	using namespace ShitGraph;
+
+	Win32StartupGdiplus();
+
+	Win32Render renderer(instance);
+	renderer.CreateMainWindow(WndProc);
+
+	g_Graphs.AddGraph(new ExplicitFunctionGraph(Polynomial({ 0 })));
+	g_Graphs.AddGraph(new ExplicitFunctionGraph(Polynomial({ -6, 11, -6, 1 })));
+	g_Graphs.AddGraph(new ImplicitFunctionGraph(Ellipse(0, 0, 10, 10)));
+
+	const int result = renderer.Run(cmdShow);
+	Win32ShutdownGdiplus();
+
+	return result;
+}
+
+LRESULT CALLBACK WndProc(HWND handle, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+	case WM_PAINT: {
+		PAINTSTRUCT ps;
+		const HDC dc = BeginPaint(handle, &ps);
+
+		RECT clientRectApi;
+		GetClientRect(handle, &clientRectApi);
+		const ShitGraph::Rectangle clientRect = {
+			{ static_cast<ShitGraph::Scalar>(clientRectApi.left), static_cast<ShitGraph::Scalar>(clientRectApi.top) },
+			{ static_cast<ShitGraph::Scalar>(clientRectApi.right), static_cast<ShitGraph::Scalar>(clientRectApi.bottom) }
+		};
+
+		const HDC bufferDC = CreateCompatibleDC(dc);
+		const HBITMAP bitmap = CreateCompatibleBitmap(dc, clientRectApi.right, clientRectApi.bottom);
+		const HGDIOBJ oldBitmap = SelectObject(bufferDC, bitmap);
+
+		ShitGraph::Win32GraphicDevice device(handle, bufferDC, clientRect);
+		g_Graphs.Render(device);
+
+		BitBlt(dc, 0, 0, clientRectApi.right, clientRectApi.bottom, bufferDC, 0, 0, SRCCOPY);
+		SelectObject(bufferDC, oldBitmap);
+		DeleteObject(bitmap);
+		DeleteDC(bufferDC);
+
+		EndPaint(handle, &ps);
+		return 0;
+	}
+
+	case WM_LBUTTONDOWN:
+		g_MousePos.x = LOWORD(lParam);
+		g_MousePos.y = HIWORD(lParam);
+		g_IsMoving = true;
+		return 0;
+
+	case WM_MOUSEMOVE:
+		if (g_IsMoving) {
+			const ShitGraph::Point center = g_Graphs.GetCenter();
+			const WORD x = LOWORD(lParam), y = HIWORD(lParam);
+
+			g_Graphs.SetCenter({
+				center.X - (g_MousePos.x - x) * g_Graphs.GetScale(),
+				center.Y + (g_MousePos.y - y) * g_Graphs.GetScale()
+			});
+			g_MousePos.x = x;
+			g_MousePos.y = y;
+
+			InvalidateRect(handle, nullptr, true);
+		}
+		return 0;
+
+	case WM_LBUTTONUP:
+		g_IsMoving = false;
+		return 0;
+
+	case WM_MOUSEWHEEL:
+		if (static_cast<SHORT>(HIWORD(wParam)) > 0) {
+			g_Graphs.SetScale(g_Graphs.GetScale() / 2);
+		} else {
+			g_Graphs.SetScale(g_Graphs.GetScale() * 2);
+		}
+		InvalidateRect(handle, nullptr, true);
+		return 0;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(handle, message, wParam, lParam);
+}
