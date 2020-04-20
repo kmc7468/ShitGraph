ae0d4b5c65e74519c58fa848d7c4287ab078a577
kmc7468 <kmc7468@naver.com>
2020-04-20 21:41:47 +0900

    Event 구조체 삭제

--- src/Window.cpp
+++
@@ -1,9 +0,0 @@
-#include <ShitGraph/Window.hpp>
-
-namespace ShitGraph {
-	EventType Event::GetType() const noexcept {
-		return std::visit([](const auto& event) noexcept {
-			return event.Type;
-		}, m_Event);
-	}
-}
