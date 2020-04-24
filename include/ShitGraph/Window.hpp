31b2dc36d8852a9805ee7df5bfa3efbbbeec625a
kmc7468 <kmc7468@naver.com>
2020-04-24 19:38:01 +0900

    EventAdaptor 클래스 추가

---
+++ include/ShitGraph/Window.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+namespace ShitGraph {
+	class Window;
+	class GraphicDevice;
+
+	struct EventArgs {
+		ShitGraph::Window& Window;
+	};
+
+	struct PaintEventArgs final {
+		GraphicDevice& Device;
+	};
+
+	enum class MouseButton {
+		None,
+
+		LButton,
+	};
+
+	struct MouseEventArgs final {
+		int X, Y;
+		MouseButton Button;
+	};
+
+	struct MouseWheelEventArgs final : EventArgs {
+		int Delta;
+	};
+
+	struct KeyEventArgs final {
+		int Key;
+	};
+}
+
+namespace ShitGraph {
+	class EventAdaptor {
+	public:
+		EventAdaptor() noexcept = default;
+		EventAdaptor(const EventAdaptor&) = delete;
+		virtual ~EventAdaptor() = default;
+
+	public:
+		EventAdaptor& operator=(const EventAdaptor&) = delete;
+
+	public:
+		virtual void Paint(PaintEventArgs e) = 0;
+		virtual void Destroy() = 0;
+
+		virtual void MouseDown(MouseEventArgs e) = 0;
+		virtual void MouseUp(MouseEventArgs e) = 0;
+		virtual void MouseMove(MouseEventArgs e) = 0;
+		virtual void MouseWheel(MouseWheelEventArgs e) = 0;
+
+		virtual void KeyDown(KeyEventArgs e) = 0;
+	};
+}
